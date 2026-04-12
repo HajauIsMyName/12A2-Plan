48a2063170e7575c132590aeb6df47e1e37b1e5b
Hajau <hieuhau4@gmail.com>
2026-04-12 23:24:15 +0700

    update file

--- js/main.js
+++ js/main.js
@@ -1,29 +1,13 @@
-const titleText = "KỶ YẾU LỚP 12A2";
-const subText = "Thanh xuân không lưu lại. Nhưng website thì có.";
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+}, {
+    threshold: 0.2
+});
 
-let titleIndex = 0;
-let subIndex = 0;
-
-function typeTitle() {
-    if (titleIndex < titleText.length) {
-        document.getElementById("typing-title").innerHTML +=
-            titleText.charAt(titleIndex);
-        titleIndex++;
-        setTimeout(typeTitle, 80);
-    } else {
-        setTimeout(typeSubText, 300);
-    }
-}
-
-function typeSubText() {
-    if (subIndex < subText.length) {
-        document.getElementById("typing-text").innerHTML +=
-            subText.charAt(subIndex);
-        subIndex++;
-        setTimeout(typeSubText, 40);
-    }
-}
-
-window.onload = function () {
-    typeTitle();
-};
+document.querySelectorAll('.fade-up').forEach(el => {
+    observer.observe(el);
+});
--- js/preview-image.js
+++ js/preview-image.js
@@ -1,21 +1,23 @@
-const previewImages = document.querySelectorAll(".preview-img");
-const viewer = document.getElementById("imageViewer");
-const viewerImg = document.getElementById("viewerImg");
-const closeBtn = document.querySelector(".close-btn");
+document.addEventListener("DOMContentLoaded", function () {
+    const previewImages = document.querySelectorAll(".preview-img");
+    const viewer = document.getElementById("imageViewer");
+    const viewerImg = document.getElementById("viewerImg");
+    const closeBtn = document.querySelector(".close-btn");
 
-previewImages.forEach(img => {
-    img.addEventListener("click", () => {
-        viewer.style.display = "flex";
-        viewerImg.src = img.src; // lấy ảnh gốc
+    previewImages.forEach(img => {
+        img.addEventListener("click", () => {
+            viewer.style.display = "flex";
+            viewerImg.src = img.src; // lấy ảnh gốc
+        });
     });
-});
 
-closeBtn.addEventListener("click", () => {
-    viewer.style.display = "none";
-});
-
-viewer.addEventListener("click", (e) => {
-    if (e.target === viewer) {
+    closeBtn.addEventListener("click", () => {
         viewer.style.display = "none";
-    }
+    });
+
+    viewer.addEventListener("click", (e) => {
+        if (e.target === viewer) {
+            viewer.style.display = "none";
+        }
+    });
 });
